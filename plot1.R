6ea4817036d5566ccdb6feaaa995ab4e47ebd4ae
Alberto Perea <alberto.perea.89@gmail.com>
2020-08-31 23:18:19 +0200

    Plot 1 Script

--- plot1.R
+++ plot1.R
@@ -14,5 +14,5 @@ hist(df$Global_active_power,
      main = "Global Active Power",
      xlab = "Global Active Power(kilowatts)",
      col = "red")
-dev.copy(png, file = "plot1.png") #Create PNG file
+dev.copy(png, file = "plot1.png", width = 480, height = 480) #Create PNG file
 dev.off() #Close graphic device
