c62bf7cb5f565e6d4a83ef24c9bae1b6531881fb
Alberto Perea <alberto.perea.89@gmail.com>
2020-08-31 23:18:40 +0200

    Plot 2 script

--- plot2.R
+++ plot2.R
@@ -10,10 +10,11 @@ df$Date <- as.Date(df$Date, "%d/%m/%Y")
 df$Time <- strptime(paste(df$Date, df$Time), "%Y-%m-%d %H:%M:%S")
 
 #CREATE PLOT
+
 plot(df$Time, df$Global_active_power,
      type = "l",
      main = "",
      xlab = "",
      ylab = "Global Active Power(kilowatts)")
-dev.copy(png, file = "plot2.png") #Create PNG file
+dev.copy(png, file = "plot2.png", width = 480, height = 480) #Create PNG file
 dev.off() #Close graphic device
